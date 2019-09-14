9d3b949f42d2baab890a4bbafa412fef59084bb9
jkim <jihyo.kim@move.com>
2019-09-14 11:44:02 -0700

    added components

---
+++ src/components/SampleComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography} from '@material-ui/core';
+
+const SampleComponent = () => {
+    return (
+        <div>
+            <Typography>
+                Sample Component
+            </Typography>
+        </div>
+    );
+} 
+
+export default SampleComponent;
